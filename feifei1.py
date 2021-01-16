093b539a47131ef228f365d3ac07998c134051c1
lucas <lucas@surface>
2021-01-16 19:22:52 +0800

    太大太小不对

--- feifei1.py
+++ feifei1.py
@@ -1,11 +1,16 @@
 import random
 my_number = random.randint(1,100)
-for i in range(15):
-    a = eval(input("number:"))
-    if a<my_number:
-        print("小")
-    elif a>my_number:
-        print("大")
+print(my_number)
+for i in range(100):
+    x = eval(input("number:"))
+    cha=my_number-x
+    if x<my_number:
+        print("太小")
+        if cha<+20:
+            print("小")
+    elif x>my_number:
+        print("太大")
+        if cha>-20:
+            print("大")
     else:
         print("对")
-    
