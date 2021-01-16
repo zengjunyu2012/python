b2e2041dcfbd9b99520c2e275eb2aad4b381fd48
lucas <lucas@surface>
2021-01-16 16:48:54 +0800

    add diary

--- feifei1.py
+++ feifei1.py
@@ -1,6 +1,6 @@
 import random
 my_number = random.randint(1,100)
-for i in range(100000):
+for i in range(15):
     a = eval(input("number:"))
     if a<my_number:
         print("小")
@@ -8,3 +8,4 @@ for i in range(100000):
         print("大")
     else:
         print("对")
+    
