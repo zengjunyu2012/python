2530e56c3b3a16303c2b0cce2996e944407f9c82
lucas <lucas@surface>
2021-01-22 11:52:49 +0800

    以前出错现在改正

--- feifei1.py
+++ feifei1.py
@@ -3,11 +3,17 @@ my_number = random.randint(1,100)
 print(my_number)
 for i in range(100):
     you_input = eval(input("number:"))
-    cha=you_input-my_number
-if you_input==my_number:
+if you_input=my_number:
     print("对")
 else:
     if you_input<my_number:
-        print("小")
+        if you_input<my_number:
+            print("太小")
+        else:
+            print("小")
     else:
-        print("大")
+        if you_input-20>my_number:
+            print("太大")
+        else:
+            print("大")
+            
