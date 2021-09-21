29c1071b87b3888ff02801115cedb4bd731c62b5
lucas <lucas@surface>
2021-09-21 15:48:33 +0800

    f

--- ff2.py
+++ ff2.py
@@ -1,1 +1,20 @@
-
+<html>
+  <head>
+   <title>head First Lounge</title>
+  </head>
+  <body>
+    <h1>welcome to the Head First Iounge</h1>
+    <img src="drinks>gif">
+    <p>
+    	Join us any evening for refreshing elixirs,
+    	conversation and maybe a game or
+    	two of <em>Dance Dance Revolution</em>.
+    	Wireless access is always provlded;
+    	BYOWS (Bring your own Wed server).
+    </p>
+    <h2>Direxrions</h2>
+    <p>you'll find us right in the center of downtown webville. Come joln us!
+    </p>
+  </body>
+</html>
+    
